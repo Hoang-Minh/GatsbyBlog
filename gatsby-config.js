d1afc7270ae945349d4fdbf66bd0bab64e5c6c3f
Hoang-Minh <quataoxanh83@gmail.com>
2020-07-03 14:53:19 -0700

    generate slug for node

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,17 @@ module.exports = {
     title: "Full-Stack Bootcamp!",
     author: "Minh Nguyen",
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+    },
+  ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,40 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const blogs = data.allMarkdownRemark.edges
+
   return (
     <div>
       <Layout>
         <h1>My Blog</h1>
         <p>Post will show up here at a later time</p>
+        <ol>
+          {blogs.map(blog => {
+            return (
+              <li>
+                <h2>{blog.node.frontmatter.title}</h2>
+                <p>{blog.node.frontmatter.date}</p>
+              </li>
+            )
+          })}
+        </ol>
       </Layout>
     </div>
   )
